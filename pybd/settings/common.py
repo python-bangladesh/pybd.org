e0f12922e6abac2fd2ef200d150876554fb08474
Shakib Hossain <shakib609@gmail.com>
2016-12-16 18:03:25 +0600

    Make settings file modular to fix PR#3

--- pybd/settings/common.py
+++ pybd/settings/common.py
@@ -22,9 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'o33yvx*4!lm6ry%%qxqybmd%m5p53bwp(9$8@i059elcova@h6'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
---
+++ pybd/settings/local.py
@@ -0,0 +1,5 @@
+from .common import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
